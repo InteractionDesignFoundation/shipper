a84d7f0428a61d2c7707c7da1f1f2c46c89410e1
Alies Lapatsin <web.lapateen@gmail.com>
2019-05-04 16:20:00 +0300

    #18 Extract checklist into a new component

--- src/App.vue
+++ src/App.vue
@@ -33,24 +33,7 @@
       class="step"
       @release-created="onReleaseCreated"/>
 
-    <section
-      v-if="createdRelease"
-      class="step">
-      <h2 class="title">🏁 That's it, you are awesome! 🦄</h2>
-      <ol class="list">
-        <li>
-          <strong>Please stay online for the next 1+ hour(s)</strong> to solve possible errors caused by the new release.
-          <ol class="list">
-            <li>In case of CI build errors, we will be notified on <code>#dev</code> channel ("[Build failed]")</li>
-            <li>In case of deploy errors, we will be notified on <code>#dev</code> channel ("Failed Deployment")</li>
-            <li>In case of important runtime errors, we will be notified on <code>#error--production</code> and <code>#js-errors-production</code> channels</li>
-            <li>In case of success, you will receive a notification on <code>#announcements</code> channel</li>
-          </ol>
-        </li>
-        <li>Check <a href="https://app.codeship.com/projects/79159" target="_blank">app status on Codeship CI</a></li>
-        <li>Check <a href="https://github.com/InteractionDesignFoundation/IDF-web/blob/develop/docs/workflows/deployment/checklist--after-deployment.md" target="_blank">After Deployment Checklist</a> </li>
-      </ol>
-    </section>
+    <AfterDeployChecklist v-if="createdRelease"/>
   </main>
 </template>
 
@@ -60,6 +43,7 @@
   import MilestoneSelector from './components/MilestoneSelector.vue'
   import Milestone from './components/Milestone.vue'
   import Release from './components/Release.vue'
+  import AfterDeployChecklist from './components/AfterDeployChecklist.vue'
 
   export default {
     name: 'App',
@@ -68,6 +52,7 @@
       MilestoneSelector,
       Milestone,
       Release,
+      AfterDeployChecklist,
     },
     data() {
       return {
---
+++ src/components/AfterDeployChecklist.vue
@@ -0,0 +1,19 @@
+<template>
+  <section
+    class="step">
+    <h2 class="title">🏁 That's it, you are awesome! 🦄</h2>
+    <ol class="list">
+      <li>
+        <strong>Please stay online for the next 1+ hour(s)</strong> to solve possible errors caused by the new release.
+        <ol class="list">
+          <li>In case of CI build errors, we will be notified on <code>#dev</code> channel ("[Build failed]")</li>
+          <li>In case of deploy errors, we will be notified on <code>#dev</code> channel ("Failed Deployment")</li>
+          <li>In case of important runtime errors, we will be notified on <code>#error--production</code> and <code>#js-errors-production</code> channels</li>
+          <li>In case of success, you will receive a notification on <code>#announcements</code> channel</li>
+        </ol>
+      </li>
+      <li>Check <a href="https://app.codeship.com/projects/79159" target="_blank">app status on Codeship CI</a></li>
+      <li>Check <a href="https://github.com/InteractionDesignFoundation/IDF-web/blob/develop/docs/workflows/deployment/checklist--after-deployment.md" target="_blank">After Deployment Checklist</a> </li>
+    </ol>
+  </section>
+</template>
