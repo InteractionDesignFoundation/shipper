719a8a59fc69cfe53ea8436ba5993ef41d8cd51a
Hasan Wajahat <hasan_wajahat@hotmail.com>
2022-03-02 15:38:58 +0500

    Use correct vue app creation method

--- src/main.js
+++ src/main.js
@@ -1,9 +1,5 @@
-import Vue from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
-
-/* eslint-disable no-new */
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+const app = createApp(App)
+app.mount('#app');
